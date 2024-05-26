7f03a12ef9116130e5f6c06003972726b00a6f3e
HlexNC <hlexhelftd@gmail.com>
2024-05-26 04:21:42 +0000

    feat: Remove unnecessary Swagger documentation from API routes

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/**
- * @swagger
- * /api/greet:
- *   get:
- *     description: Greet the user
- *     responses:
- *       200:
- *         description: Returns a greeting message
- */
 router.get('/greet', function(req, res, next) {
   res.json({ message: 'Hello, welcome to the Devablos Project V2 API!' });
 });
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/**
- * @swagger
- * /:
- *   get:
- *     description: Welcome to the API
- *     responses:
- *       200:
- *         description: Returns a welcome message
- */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });

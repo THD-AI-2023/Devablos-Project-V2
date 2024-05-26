7b9a819c89a5f4e2c97b0a40b0ca587ffc638aa6
HlexNC <hlexhelftd@gmail.com>
2024-05-26 03:17:54 +0000

    chore: updated dependencies.

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ const openaiRouter = require('./routes/openaiRoutes');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());

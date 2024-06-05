875b721a56a612d0c8c408c6294ae9b0f861a638
HlexNC <hlexhelftd@gmail.com>
2024-06-05 07:37:54 +0000

    Set up a test for the WebSockets

--- client-ws/src/App.js
+++ client-ws/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
-import useWebSocket from 'react-use-websocket';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-const WS_URL = `ws://${process.env.CODESPACE_NAME}-${process.env.PORT || '5001'}.app.github.dev`;
+const WS_URL = `wss://localhost:5002`;
+
+console.log('WebSocket URL:', WS_URL);
 
 function App() {
-  useWebSocket(WS_URL, {
+  const { readyState } = useWebSocket(WS_URL, {
     onOpen: () => {
       console.log('WebSocket connection established.');
+    },
+    onError: (error) => {
+      console.error('WebSocket error:', error);
     }
   });
+
+  console.log('WebSocket state:', ReadyState[readyState]);
 
   return (
     <div>Hello WebSockets!</div>

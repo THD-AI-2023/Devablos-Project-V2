21631ba57b580173bf5498f2729692752b9ff201
HlexNC <hlexhelftd@gmail.com>
2024-06-05 11:45:48 +0000

    Updated the .env and WebSocket Secure client example.

--- client-wss/src/App.js
+++ client-wss/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-const WS_URL = `wss://localhost:5002`;
+const WSS_URL = process.env.REACT_APP_WSS_URL || `wss://${window.location.hostname}/ws`;
 
-console.log('WebSocket URL:', WS_URL);
+console.log('WebSocket URL:', WSS_URL);
 
 function App() {
-  const { readyState } = useWebSocket(WS_URL, {
+  const { readyState } = useWebSocket(WSS_URL, {
     onOpen: () => {
       console.log('WebSocket connection established.');
     },

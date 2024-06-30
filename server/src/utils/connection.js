138b5317602941c92a00a20dcb471e5c6e40b1ea
Denisa <112165840+Reptilian6163@users.noreply.github.com>
2024-06-30 14:22:29 +0200

    Update connection.js

--- server/src/utils/connection.js
+++ server/src/utils/connection.js
@@ -1,2 +1,48 @@
-const clients = new Map();
+const fs = require('fs');
+const path = require('path');
+
+class PersistentMap extends Map {
+  constructor(filePath) {
+    super();
+    this.filePath = filePath;
+    this.load();
+  }
+
+  set(key, value) {
+    super.set(key, value);
+    this.save();
+    return this;
+  }
+
+  delete(key) {
+    const result = super.delete(key);
+    this.save();
+    return result;
+  }
+
+  clear() {
+    super.clear();
+    this.save();
+  }
+
+  load() {
+    try {
+      const data = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+      for (const [key, value] of Object.entries(data)) {
+        super.set(key, value);
+      }
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        console.error('Error loading data:', error);
+      }
+    }
+  }
+
+  save() {
+    const data = Object.fromEntries(this);
+    fs.writeFileSync(this.filePath, JSON.stringify(data), 'utf8');
+  }
+}
+
+const clients = new PersistentMap(path.join(__dirname, 'clients.json'));
 module.exports = clients;
